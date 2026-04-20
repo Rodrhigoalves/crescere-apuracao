e90f45c2489134276f08720e4fa9e970fbd94859
Rodrhigoalves <72764829+Rodrhigoalves@users.noreply.github.com>
2026-04-19 21:25:44 -0300

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -3,12 +3,13 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def enviar_email(destinatario, assunto, corpo):
-    # DADOS DO GMAIL (Substitua apenas aqui no seu PC)
     SMTP_SERVER = "smtp.gmail.com"
     SMTP_PORT = 587
-    SMTP_USER = "agendamentodeferias@gmail.com"  # <--- Seu Gmail novo
-    SMTP_PASSWORD = "xebn nybi ilpm lodo"      # <--- Sua Senha de App de 16 dígitos
+    SMTP_USER = "seu-email@gmail.com"
+    SMTP_PASSWORD = "sua-senha-de-app" # Certifique-se de que são 16 letras sem espaços aqui
 
+    if not destinatario or "@" not in str(destinatario):
+        return False
     try:
         msg = MIMEMultipart()
         msg['From'] = SMTP_USER
@@ -17,11 +18,11 @@ def enviar_email(destinatario, assunto, corpo):
         msg.attach(MIMEText(corpo, 'plain'))
 
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-        server.starttls()  # Segurança obrigatória
+        server.starttls()
         server.login(SMTP_USER, SMTP_PASSWORD)
         server.send_message(msg)
         server.quit()
         return True
     except Exception as e:
-        print(f"Erro ao enviar e-mail: {e}")
+        print(f"Erro: {e}")
         return False
